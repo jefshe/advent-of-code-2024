8982606a3b403d29ad15b8667af0083d5623948d
jefshe <jeffrey.shen@kauza.dk>
2024-12-11 08:56:30 +0100

    day11: cache not good enough. need to memo

--- rust/aoc_2024/src/days/day11/mod.rs
+++ rust/aoc_2024/src/days/day11/mod.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+
 use super::{time_run, Answer, TX};
 use crate::BoxedAsync;
 use crate::{util::*, ItemTX};
@@ -11,52 +14,76 @@ async fn run(mut tx: ItemTX) -> Result<()> {
 }
 
 pub fn parta(_tx: &mut ItemTX) -> String {
-    let mut stones = input();
-    blink(&mut stones, 25);
-    format!("{:?}", stones.len())
+    // let mut stones = input();
+    // let mut cache: HashMap<usize, Rc<Vec<usize>>> = HashMap::new();
+    // cache.insert(0, Rc::new(vec![1]));
+    // cache.insert(1, Rc::new(vec![2024]));
+    // cache.insert(2024, Rc::new(vec![20, 24]));
+    // cache.insert(20, Rc::new(vec![2, 0]));
+    // cache.insert(24, Rc::new(vec![2, 4]));
+    // format!("{:?}", blink(&mut stones, &mut cache, 25))
+    "see part a".to_string()
 }
 
-pub fn partb(_tx: &mut ItemTX) -> String {
+pub fn partb(tx: &mut ItemTX) -> String {
     let mut stones = input();
-    blink(&mut stones, 75);
-    format!("{:?}", stones.len())
+    let mut cache: HashMap<usize, Rc<Vec<usize>>> = HashMap::new();
+    cache.insert(0, Rc::new(vec![1]));
+    cache.insert(1, Rc::new(vec![2024]));
+    cache.insert(2024, Rc::new(vec![20, 24]));
+    cache.insert(20, Rc::new(vec![2, 0]));
+    cache.insert(24, Rc::new(vec![2, 4]));
+    format!("{:?}", blink(&mut stones, &mut cache, 75, tx))
 }
 
-pub fn blink(stones: &mut Vec<usize>, blinks: usize) {
+///
+/// 0
+/// 1
+/// 2024
+/// 20 24
+/// 2 0 2 4
+
+pub fn blink(
+    stones: &Vec<usize>,
+    cache: &mut HashMap<usize, Rc<Vec<usize>>>,
+    blinks: usize,
+    tx: &mut ItemTX,
+) -> usize {
     if blinks == 0 {
-        return;
+        return stones.len();
     }
-    let mut blinks_left = blinks;
 
-    while blinks_left > 0 {
-        let mut i = 0;
-        while i < stones.len() {
-            match stones[i] {
-                0 => stones[i] = 1,
-                num if digit_count(num) % 2 == 0 => {
-                    let split_at = digit_count(num) / 2;
-                    let left = num / 10_usize.pow(split_at as u32);
-                    let right = num % 10_usize.pow(split_at as u32);
-                    stones.insert(i, left);
-                    stones[i + 1] = right;
-                    i += 1
-                }
-                _ => stones[i] *= 2024,
+    let mut i = 0;
+    let mut total_length = 0;
+    tx.append(format!("{:?}  @ blink {blinks}", stones))
+        .expect("Unable to append");
+    while i < stones.len() {
+        match (stones[i], cache.get(&stones[i])) {
+            (_, Some(next_stones)) => {
+                total_length += blink(&next_stones.clone(), cache, blinks - 1, tx);
             }
-            i += 1
+            (num, _) if digit_count(num) % 2 == 0 => {
+                let split_at = digit_count(num) / 2;
+                let left = num / 10_usize.pow(split_at as u32);
+                let right = num % 10_usize.pow(split_at as u32);
+                let next_stones = Rc::new(vec![left, right]);
+                cache.insert(num, next_stones.clone());
+                total_length += blink(&next_stones, cache, blinks - 1, tx);
+            }
+            (num, _) => total_length += blink(&vec![num * 2024], cache, blinks - 1, tx),
         }
-        blinks_left -= 1
+        i += 1
     }
+    total_length
 }
 
 pub fn input() -> Vec<usize> {
-    let mut vec = parse_lines_iter("day11")
+    let vec = parse_lines_iter("day11")
         .next()
         .unwrap()
         .split(" ")
         .map(|s| s.parse().expect("Invalid number"))
         .collect::<Vec<usize>>();
-    vec.reserve(1_000_000);
     vec
 }
 
